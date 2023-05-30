85c62a05d84e1b5b1c843ab0cb98c1e6a8e9d451
Gary_Desktop <b07501047@ntu.edu.tw>
2023-05-30 13:07:25 +0800

    move some files

--- compile_ui.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Compile all the .ui files in ./resources to .py files with pyuic5 and ouput to ./views
-
-for file in ./resources/ui/*.ui
-do
-    pyuic5 $file -o ./views/$(basename $file .ui)_ui.py
-done
